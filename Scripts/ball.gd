1b96f9569ccd598b81c2009d0fa1ddf654789641
Rafael Almeida <apprafael.almeida@gmail.com>
2025-01-07 21:15:52 -0300

    ball movement

---
+++ Scripts/ball.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+
+@export var y_speed = -250.0
+@export var angle = [250,-250]
+var started = false
+
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept") and !started:
+		playGame()
+
+	var collision = move_and_collide(velocity*delta)
+	if collision != null:
+		velocity = velocity.bounce(collision.get_normal())
+	
+
+func playGame():
+	started = true
+	velocity = Vector2(angle.pick_random(), y_speed)
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,7 +1,6 @@
 extends CharacterBody2D
 
-const SPEED = 400.0
-
+@export var SPEED = 400.0
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
@@ -11,4 +10,4 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+	move_and_collide(velocity*delta)
