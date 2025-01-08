a024f81a2c1d925d31185f04882b7eb324fa7b91
Rafael Almeida <apprafael.almeida@gmail.com>
2025-01-07 21:35:52 -0300

    block destruction

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -9,12 +9,15 @@ var started = false
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and !GameManager.started:
 		playGame()
-
+	
 	var collision = move_and_collide(velocity*delta)
+	
 	if collision != null:
 		velocity = velocity.bounce(collision.get_normal())
+		if "block" in collision.get_collider().name:
+			collision.get_collider().queue_free()
 	
 
 func playGame():
 	GameManager.started = true
-	velocity = Vector2(angle.pick_random(), y_speed)
+	velocity = Vector2(angle.pick_random(), y_speed)			
