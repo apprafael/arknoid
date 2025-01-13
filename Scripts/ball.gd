d3c69fb8933ac7ebc39758311bb52ffbb73fd35d
Rafael Almeida <apprafael.almeida@gmail.com>
2025-01-13 12:07:25 -0300

    spawn items and collision rules

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
-
 @export var y_speed = -250.0
 @export var angle = [250,-250]
-var started = false
+@export var drop_probability = 0.2
+@export var item_scene = preload("res://Scenes/power_up.tscn")
 
 
 func _physics_process(delta: float) -> void:
@@ -16,8 +16,15 @@ func _physics_process(delta: float) -> void:
 		velocity = velocity.bounce(collision.get_normal())
 		if "block" in collision.get_collider().name:
 			collision.get_collider().queue_free()
+			spawnItem(collision.get_position())
 	
 
 func playGame():
 	GameManager.started = true
 	velocity = Vector2(angle.pick_random(), y_speed)			
+
+func spawnItem(position_item: Vector2):
+	if randf() < drop_probability:
+		var item = item_scene.instantiate()
+		item.position = position_item
+		get_parent().add_child(item)
